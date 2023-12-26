610f25047a36a36eff3138990868d308f592621e
OstapoKapo <OstapoKapo@gmail.com>
2023-12-26 19:11:26 +0200

    end login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,15 @@
 import { FC, useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import './Login.scss';
+import CryptoJS from "crypto-js";
+import { useNavigate } from 'react-router-dom';
 
 
 const Login: FC = () => {
+
+    const navigate = useNavigate()
+
+    const secretPass: string = "XkhZG4fW2t2W";
 
     const [data, setData] = useState({
         login: "",  // User login
@@ -19,11 +25,47 @@ const Login: FC = () => {
         }));
     };
 
+    const decrypt = (name: any, password: any): string[] => {
+        const bytesForName : any = CryptoJS.AES.decrypt(name, secretPass);
+        const decryptName: string = JSON.parse(bytesForName.toString(CryptoJS.enc.Utf8));
+
+        const bytesForPassword : any = CryptoJS.AES.decrypt(password, secretPass);
+        const decryptPassword: string = JSON.parse(bytesForPassword.toString(CryptoJS.enc.Utf8));
+        decryptPassword.toLowerCase();
+        decryptName.toLowerCase();
+        return([decryptName, decryptPassword]);
+    }
+
+    interface UserData {
+        login: string,
+        password: string
+    }
+
     // post function to server (send data)
-    const login = (e:any) : void => {
+    const login = async  (e:any) => {
         e.preventDefault();
         try {
-            // axios.post('server url', {data})
+            await axios.post('http://localhost:8000/createUser', {})
+                 .then((response) => {
+                    if(response.status !== 400){
+                        let adminData = decrypt(response.data.name, response.data.password);
+                        let userData: UserData = {
+                            login: data.login.toLowerCase(),
+                            password: data.password.toLowerCase()
+                        };
+                        console.log(userData)
+                        if(userData.login === adminData[0]&& userData.password === adminData[1]){
+                            navigate('/mainPage')
+                        }else{
+                            alert('Incorrect password or name')
+                        }
+                    }else{
+                        console.log('error');
+                    }
+                 })
+                 .catch((error) => {
+                     console.error('Error:', error);
+                 });
         } catch(error) {
             console.error(error)
         }
