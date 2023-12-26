5d814bc75e03e7ea371d9139cf49fe13c490ff76
UserName <m492615892gmail.com>
2023-12-26 16:03:40 +0200

    Add styles to Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,10 @@ import './Card.scss';
 
 const Card = () => {
     return (
-        <div className='Card'></div>
+        <div className='Card'>
+            <h3>Відмазка</h3>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem optio sit! Sint, aliquam saepe!</p>
+        </div>
     )
 }
 
