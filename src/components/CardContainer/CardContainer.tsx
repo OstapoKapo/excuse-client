26a2f16b4b753af372762f0ec590aa16f22af1ec
Andriy_Step <andystep2008@gmail.com>
2023-12-26 21:05:28 +0200

    update, adding excuse

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -3,21 +3,11 @@ import axios from 'axios';
 import './CardContainer.scss';
 import Card from './Card/Card';
 
-interface Vidmazka{
-    excuse: string,
-    date: string,
-    creator: string
+interface Vidmazka {
+  excuse: string;
+  date: string;
+  creator: string;
 }
-
-let array: Array<Vidmazka> = [
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'}
-]
-
 
 const CardContainer = () => {
   const getCurrentDate = (): string => {
@@ -99,14 +89,19 @@ const CardContainer = () => {
             name='exuse'
             value={data.exuse}
             onChange={handleChange}
-            />
-            <button onClick={createExuse} className="btn_adding-cards">Додати нову відмазку</button>
-            </div>
-            <div className="block_cards">
-                {array.map(item=> <Card key={item} excuse={item.excuse} creator={item.creator}/>)}
-            </div>
+          />
         </div>
-    )
-}
+        <button onClick={createExuse} className='btn_adding-cards'>
+          Додати нову відмазку
+        </button>
+      </div>
+      <div className='block_cards'>
+        {excuses.map((item, index) => (
+          <Card key={index} excuse={item.excuse} creator={item.creator} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CardContainer;
