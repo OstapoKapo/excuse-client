afa06e0533532e04da3e7543217db08a6d5010d2
UserName <m492615892gmail.com>
2023-12-26 15:35:56 +0200

    Add components Header , CardContainer , Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import './Card.scss';
+
+const Card = () => {
+    return (
+        <div className='Card'></div>
+    )
+}
+
+export default Card
---
+++ src/components/CardContainer/CardContainer.tsx
@@ -0,0 +1,8 @@
+import './CardContainer.scss';
+const CardContainer = () => {
+    return (
+        <div className='CardContainer'></div>
+    )
+}
+
+export default CardContainer
