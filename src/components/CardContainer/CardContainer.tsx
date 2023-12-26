3470d3eb8e6b0bd22ee4fcb26b12307970ffe4b2
Andriy_Step <andystep2008@gmail.com>
2023-12-26 21:00:25 +0200

    adding excuse to server

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,95 +1,107 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './CardContainer.scss';
 import Card from '../Card/Card';
 
-interface Vidmazka{
-    excuse: string,
-    date: string,
-    creator: string
+interface Vidmazka {
+  excuse: string;
+  date: string;
+  creator: string;
 }
-
-let array: Array<Vidmazka> = [
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'},
-    {creator: 'admin', date: '26/12/2023', excuse: 'Я забув'}
-]
-
 
 const CardContainer = () => {
+  const getCurrentDate = (): string => {
+    const currentDate: Date = new Date();
 
-    const getCurrentDate = () : string => {
-        const currentDate: Date = new Date();
-    
-        const day: string = currentDate.getDate().toString().padStart(2, '0');
-        const month: string = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-        const year: number = currentDate.getFullYear();
-    
-        return `${month}/${day}/${year}`;
+    const day: string = currentDate.getDate().toString().padStart(2, '0');
+    const month: string = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const year: number = currentDate.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  };
+
+  const [data, setData] = useState({
+    creator: '', // author's name
+    exuse: '', // excuse
+    date: getCurrentDate(),
+  });
+
+  const [excuses, setExcuses] = useState<Array<Vidmazka>>([]);
+
+  useEffect(() => {
+    const fetchExcuses = async () => {
+        try {
+          const response = await axios.get('http://localhost:8000/api/exuses');
+          setExcuses(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+      
+          if (axios.isAxiosError(error)) {
+            console.error('Error response:', error.response);
+          }
+        }
       };
-    
-      const [data, setData] = useState({
-        creator: "",  // author's name
-        exuse: "",  // exuse
-        date: getCurrentDate(),
+
+    fetchExcuses();
+  }, []);
+
+  const handleChange = (e: any): void => {
+    const { name, value } = e.target;
+    setData((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const createExuse = async (e: any) => {
+    e.preventDefault();
+    console.log(data);
+    try {
+      await axios.post('http://localhost:8000/createExuse', {
+        creator: data.creator,
+        date: data.date,
+        excuse: data.exuse,
       });
 
-
-    // Handles changes in inputs data
-    const handleChange = (e: any) : void => {
-        const { name, value } = e.target;
-        setData((prevValues) => ({
-          ...prevValues,
-          [name]: value,
-        }));
-    };
-
-    const createExuse = async  (e:any) => {
-        e.preventDefault();
-        console.log(data);
-        try {
-            await axios.post('http://localhost:8000/createExuse', {creator: data.creator, date: data.date, excuse: data.exuse})
-                 .then((response) => {
-                    console.log(response)
-                 })
-                 .catch((error) => {
-                     console.error('Error:', error);
-                 });
-        } catch(error) {
-            console.error(error)
-        }
+      const response = await axios.get('http://localhost:8000/api/exuses');
+      setExcuses(response.data);
+    } catch (error) {
+      console.error('Error:', error);
     }
+  };
 
-
-    return (
-        <div className='CardContainer'>
-            <div className="block_adding-cards">
-            <input
-            type="text"
-            className="input_adding-cards"
-            placeholder="Автор..."
-            name="creator"
+  return (
+    <div className='CardContainer'>
+      <div className='block_adding-cards'>
+        <div className='for_inputs'>
+          <input
+            type='text'
+            className='input_adding-cards'
+            placeholder='Автор...'
+            name='creator'
             value={data.creator}
             onChange={handleChange}
-            />
-            <input
-            type="text"
-            className="input_adding-cards"
-            placeholder="Відмазка..."
-            name="exuse"
+          />
+          <input
+            type='text'
+            className='input_adding-cards'
+            placeholder='Відмазка...'
+            name='exuse'
             value={data.exuse}
             onChange={handleChange}
-            />
-            <button onClick={createExuse} className="btn_adding-cards">Додати нову відмазку</button>
-            </div>
-            <div className="block_cards">
-                {array.map(item=> <Card key={item} excuse={item.excuse} creator={item.creator}/>)}
-            </div>
+          />
         </div>
-    )
-}
+        <button onClick={createExuse} className='btn_adding-cards'>
+          Додати нову відмазку
+        </button>
+      </div>
+      <div className='block_cards'>
+        {excuses.map((item, index) => (
+          <Card key={index} excuse={item.excuse} creator={item.creator} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CardContainer
+export default CardContainer;
