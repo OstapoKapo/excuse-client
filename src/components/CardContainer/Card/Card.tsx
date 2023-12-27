3f14b2f95f441dd1aef66b2f7c69771280c3ba1f
UserName <m492615892gmail.com>
2023-12-27 15:53:26 +0200

    add deleting Card

--- src/components/CardContainer/Card/Card.tsx
+++ src/components/CardContainer/Card/Card.tsx
@@ -1,37 +1,45 @@
+
+import { FC } from 'react';
 import './Card.scss';
-import AuthorImg from  './img/author.png';
+import AuthorImg from './img/author.png';
 import DateImg from './img/date.png';
 import PanImg from './img/pen.png';
 import DeleteImg from './img/delete.png';
+interface CardProp {
+    idObject: string;
+    creator: string;
+    excuse: string;
+    onDelete: (idObject: string) => void;
+}
 
-const Card = (props: any) => {
+const Card: FC<CardProp> = ({ idObject, creator, excuse, onDelete }) => {
     return (
         <div className='card'>
             <div className="card__author">
                 <div className="card__author__img">
                     <img src={AuthorImg} alt="" />
                 </div>
-                <div className="card__author__name">{props.creator}</div>
+                <div className="card__author__name">{creator}</div>
             </div>
             <div className="card__main">
-                <div className="card__text">{props.excuse}</div>
+                <div className="card__text">{excuse}</div>
             </div>
             <div className="card__data">
                 <div className="card__data__img">
                     <img src={DateImg} alt="" />
                 </div>
-                <div className="card__text">{props.date}</div>
+                <div className="card__text">{/* Add date here */}</div>
             </div>
             <div className="card__buttons">
                 <div className="card__buttons__btn">
                     <img src={PanImg} alt="" />
                 </div>
-                <div className="card__buttons__btn">
+                <div className="card__buttons__btn" onClick={() => onDelete(idObject)}>
                     <img src={DeleteImg} alt="" />
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Card
+export default Card;
--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,52 +1,165 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import './CardContainer.scss';
+// import Card from './Card/Card';
+
+// // interface Vidmazka {
+// //   excuse: string;
+// //   date: string;
+// //   creator: string;
+// // }
+
+// const CardContainer = () => {
+//   const getCurrentDate = (): string => {
+//     const currentDate: Date = new Date();
+
+//     const day: string = currentDate.getDate().toString().padStart(2, '0');
+//     const month: string = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+//     const year: number = currentDate.getFullYear();
+
+//     return `${month}/${day}/${year}`;
+//   };
+
+//   const [data, setData] = useState({
+//     creator: '', // author's name
+//     exuse: '', // excuse
+//     date: getCurrentDate(),
+//   });
+
+//   const [excuses, setExcuses] = useState<Array<any>>([]);
+
+//   useEffect(() => {
+//     const fetchExcuses = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:8000/api/exuses');
+//         setExcuses(response.data);
+//         console.log(response.data)
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+
+//         if (axios.isAxiosError(error)) {
+//           console.error('Error response:', error.response);
+//         }
+//       }
+//     };
+
+//     fetchExcuses();
+//   }, []);
+
+//   const handleChange = (e: any): void => {
+//     const { name, value } = e.target;
+//     setData((prevValues) => ({
+//       ...prevValues,
+//       [name]: value,
+//     }));
+//   };
+
+//   const createExuse = async (e: any) => {
+//     e.preventDefault();
+//     console.log(data);
+//     try {
+//       await axios.post('http://localhost:8000/createExuse', {
+//         creator: data.creator,
+//         date: data.date,
+//         excuse: data.exuse,
+//       });
+
+//       const response = await axios.get('http://localhost:8000/api/exuses');
+//       setExcuses(response.data);
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
+
+//   const deleteExuse = async (exuseId: string) => {
+//     try {
+//       await axios.post(`http://localhost:8000/deleteExuse/${exuseId}`);
+//     }
+//     catch (error) {
+//       console.log('Error deleting', error)
+//     }
+
+
+//   }
+
+//   return (
+//     <div className='CardContainer'>
+//       <div className='block_adding-cards'>
+//         <div className='for_inputs'>
+//           <input
+//             type='text'
+//             className='input_adding-cards'
+//             placeholder='Автор...'
+//             name='creator'
+//             value={data.creator}
+//             onChange={handleChange}
+//           />
+//           <input
+//             type='text'
+//             className='input_adding-cards'
+//             placeholder='Відмазка...'
+//             name='exuse'
+//             value={data.exuse}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <button onClick={createExuse} className='btn_adding-cards'>
+//           Додати нову відмазку
+//         </button>
+//       </div>
+//       <div className='block_cards'>
+//         {excuses.map((item, index) => (
+//           <Card key={index} idObject={item._id} excuse={item.excuse} creator={item.creator} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CardContainer;
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './CardContainer.scss';
 import Card from './Card/Card';
+import './CardContainer.scss';
 
-// interface Vidmazka {
-//   excuse: string;
-//   date: string;
-//   creator: string;
-// }
+interface Exuse {
+  _id: string;
+  creator: string;
+  excuse: string;
+  date: string;
+}
 
-const CardContainer = () => {
+const CardContainer: React.FC = () => {
   const getCurrentDate = (): string => {
     const currentDate: Date = new Date();
-
     const day: string = currentDate.getDate().toString().padStart(2, '0');
     const month: string = (currentDate.getMonth() + 1).toString().padStart(2, '0');
     const year: number = currentDate.getFullYear();
-
     return `${month}/${day}/${year}`;
   };
 
   const [data, setData] = useState({
-    creator: '', // author's name
-    exuse: '', // excuse
+    creator: '',
+    exuse: '',
     date: getCurrentDate(),
   });
 
-  const [excuses, setExcuses] = useState<Array<any>>([]);
+  const [excuses, setExcuses] = useState<Exuse[]>([]);
 
   useEffect(() => {
     const fetchExcuses = async () => {
-        try {
-          const response = await axios.get('http://localhost:8000/api/exuses');
-          setExcuses(response.data);
-          console.log(response.data)
-        } catch (error) {
-          console.error('Error fetching data:', error);
-      
-          if (axios.isAxiosError(error)) {
-            console.error('Error response:', error.response);
-          }
-        }
-      };
+      try {
+        const response = await axios.get<Exuse[]>('http://localhost:8000/api/exuses');
+        setExcuses(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
     fetchExcuses();
   }, []);
 
-  const handleChange = (e: any): void => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
     setData((prevValues) => ({
       ...prevValues,
@@ -54,9 +167,8 @@ const CardContainer = () => {
     }));
   };
 
-  const createExuse = async (e: any) => {
+  const createExuse = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log(data);
     try {
       await axios.post('http://localhost:8000/createExuse', {
         creator: data.creator,
@@ -64,10 +176,20 @@ const CardContainer = () => {
         excuse: data.exuse,
       });
 
-      const response = await axios.get('http://localhost:8000/api/exuses');
+      const response = await axios.get<Exuse[]>('http://localhost:8000/api/exuses');
       setExcuses(response.data);
     } catch (error) {
       console.error('Error:', error);
+    }
+  };
+
+  const deleteExuse = async (exuseId: string) => {
+    try {
+      await axios.delete(`http://localhost:8000/deleteExuse/${exuseId}`);
+      const updatedExcuses = excuses.filter(item => item._id !== exuseId);
+      setExcuses(updatedExcuses);
+    } catch (error) {
+      console.log('Error deleting', error);
     }
   };
 
@@ -98,7 +220,13 @@ const CardContainer = () => {
       </div>
       <div className='block_cards'>
         {excuses.map((item, index) => (
-          <Card key={index} idObject={item._id} excuse={item.excuse} creator={item.creator} />
+          <Card
+            key={index}
+            idObject={item._id}
+            excuse={item.excuse}
+            creator={item.creator}
+            onDelete={() => deleteExuse(item._id)}
+          />
         ))}
       </div>
     </div>
@@ -106,3 +234,4 @@ const CardContainer = () => {
 };
 
 export default CardContainer;
+
